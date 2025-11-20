c15c308584e405f8a2faa270cec7a7e6d4e9ff77
yousefpx <190332698+QuantamPX@users.noreply.github.com>
2025-11-20 15:41:09 -0600

    complete

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const computerScoreDisplay = document.querySelector(".computer_score");
 const tieDisplay = document.querySelector(".tie_counter");
 const result = document.querySelector(".result");
 
+
+const rockBtn = document.querySelector("#rock_btn");
+const paperBtn = document.querySelector("#paper_btn");
+const scissorsBtn = document.querySelector("#scissors_btn");
+
+const playAgainBtn = document.querySelector("#play_again");
+
+
+
 const choices = ['rock', 'paper', 'scissors'];
 
 
@@ -28,7 +37,6 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
     tieDisplay.textContent = "";
-    humanChoice.toLowerCase;
     if(humanChoice === computerChoice){
     tieDisplay.textContent = "ITS A TIE!"
     }
@@ -47,26 +55,22 @@ function playRound(humanChoice, computerChoice){
     else{
         computerScore++;
         computerScoreDisplay.textContent = "COMPUTER SCORE: " + computerScore;
-    }
+    };
     if(humanScore === 5 || computerScore === 5){
         if(humanScore > computerScore){
-            result.textContent = "YOU WON";
+            result.textContent = "YOU WON"
         }
-        else if (computerScore > humanScore){
+        else if(computerScore > humanScore){
             result.textContent = "YOU LOST"
         }
         else{
-            return "GAME OVER";
+            return "GAME OVER"
         }
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
     }
 }
-//const humanSelection = getHumanChoice();
-//const computerSelection = getComputerChoice();
-
-const rockBtn = document.querySelector("#rock_btn");
-const paperBtn = document.querySelector("#paper_btn");
-const scissorsBtn = document.querySelector("#scissors_btn");
-
 rockBtn.addEventListener("click", () => {
     const computerSelection = getComputerChoice();
     playRound('rock', computerSelection);
@@ -80,5 +84,22 @@ paperBtn.addEventListener("click", () => {
 scissorsBtn.addEventListener("click", () => {
     const computerSelection = getComputerChoice();
     playRound('scissors', computerSelection);
+})
+
+function resetGame(){
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDisplay.textContent = "";
+    computerScoreDisplay.textContent = "";
+    tieDisplay.textContent = "";
+    result.textContent = "";
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+
+}
+
+playAgainBtn.addEventListener("click", () => {
+    resetGame();
 })
 
